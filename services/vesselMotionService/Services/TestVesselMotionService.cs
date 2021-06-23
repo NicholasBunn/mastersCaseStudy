d66ae9b148dde4a98fbc36db64dfe5f001ecd9a3
Nicholas Bunn <nicholasbunn04@gmail.com>
2021-06-23 18:42:20 +0200

    First test added, need  expansion

--- services/vesselMotionService/Services/TestVesselMotionService.cs
+++ services/vesselMotionService/Services/TestVesselMotionService.cs
@@ -3,9 +3,12 @@ using vesselMotionService;
 
 public class TestVesselMotionService
 {
+    VesselMotionService testInstance = new VesselMotionService(null);
+
     [Fact]
     public void TestCalculateOpenWaterRespone()
     {
-        VesselMotionService.CalculateOpenWaterResponse();
+        float result = testInstance.CalculateOpenWaterResponse(2431.24975585937F, -35.53165F, 13.5F, 337F, 212F, 1.5F);
+        Assert.Equal(result, 7.0642624F);
     }
 }
