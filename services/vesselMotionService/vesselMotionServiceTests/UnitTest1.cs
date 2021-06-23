db4e5197432b67fc86f61199803f2339e087e8c9
Nicholas Bunn <nicholasbunn04@gmail.com>
2021-06-23 15:31:19 +0200

    Code is building and running, tests not working

---
+++ services/vesselMotionService/vesselMotionServiceTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using vesselMotionService;
+
+namespace vesselMotionServiceTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {}
+
+        [Test]
+        public void TestCalculateOpenWaterRespone()
+        {}
+
+        [Test]
+        public void TestCalculateVibrationResponse()
+        {}
+    }
+}
